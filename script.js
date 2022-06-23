7c82aaf133538e0b50b95f28f67cef79417674a2
adamvaughn <adamdvaughn@gmail.com>
2022-06-22 19:21:44 -0500

    define functions

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
-const numberButtons = document.querySelectorAll('[data-number]')
-const operationButtons = document.querySelectorAll('[data-operation]')
-const equalsButton = document.querySelector('[data-equals]')
-const deleteButton = document.querySelector('[data-delete]')
-const allClearButton = document.querySelector('[data-all-clear]')
-const previousOperandTextElement = document.querySelector('[data-previous-operand]')
-const currentOperandTextElement = document.querySelector('[data-current-operand]')
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
+
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this. currentOperandTextElement = currentOperandTextElement
+        this.clear()
+    }
+};
+
+clear() {
+
+};
+
+delete() {
+
+};
+
+appendNumber(number) {
+
+};
+
+chooseOperation(operation) {
+
+};
+
+compute() {
+
+};
+
+updateDisplay() {
+
+};
